539c83ae60dbafcdedd0b9a83d0585096db29fc6
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-04 15:26:34 +0200

    started new app

---
+++ HelloThere/HelloThere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloThere
+//
+//  Created by Bheki Maenetja on 2020/06/04.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
