ab17c32684ea72cf7e06a7fee2a90bb218ed8431
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-09 14:41:16 +0200

    implemented basic search functionality

--- WebSearch/WebSearch/ViewController.swift
+++ WebSearch/WebSearch/ViewController.swift
@@ -22,6 +22,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func WebNavigate(_ sender: Any) {
+        let address: String = TxtAddress.text!
+        
+        if address == "" {
+            let alertController = UIAlertController(title: "Invalid Entry", message: "Please enter a valid url", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            return
+        } else {
+            let userURL = URL(string: address)!
+            webView.load(URLRequest(url: userURL))
+        }
+        
+        for textField in self.view.subviews where textField is UITextField {
+            textField.resignFirstResponder()
+        }
     }
     
 }
