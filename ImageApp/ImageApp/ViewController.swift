366fd36fb7ad2ceb12a5583b1fc7dc87377db20e
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-25 10:08:41 +0200

    created new project

---
+++ ImageApp/ImageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageApp
+//
+//  Created by Bheki Maenetja on 2020/06/25.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
