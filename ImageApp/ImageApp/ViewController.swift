973d37c4e9f69da1d7dc13ffd071d3cd31d487c4
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-25 10:22:06 +0200

    implemented open camera function

--- ImageApp/ImageApp/ViewController.swift
+++ ImageApp/ImageApp/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePicked: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+    }
+    
+    
+    @IBAction func saveImage(_ sender: Any) {
+    }
 }
 
