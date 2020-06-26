237a66898fd6b2ca76da9b246307e4fb96d7d557
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-26 14:46:11 +0200

    created new project

---
+++ MapApp/MapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Bheki Maenetja on 2020/06/26.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
