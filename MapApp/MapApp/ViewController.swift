6aebd3824d9e11671354217d552f035908a2bb3d
bheki-maenetja <bhekimaenetja@gmail.com>
2020-06-26 15:56:06 +0200

    completed basic location app

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -30,8 +30,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print("location: \(locValue.latitude) \(locValue.longitude)")
         let userLoc = locations.last
         let viewRegion = MKCoordinateRegion(center: (userLoc!.coordinate), latitudinalMeters: 600, longitudinalMeters: 600)
+        
+        geocode(latitude: locValue.latitude, longitude: locValue.longitude)
+        {
+            placemark, error in
+            guard let placemark = placemark, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                let address1 = placemark.thoroughfare ?? "-"
+                let city = placemark.locality ?? "-"
+                let state = placemark.administrativeArea ?? "-"
+                let zipcode = placemark.postalCode ?? "-"
+                let country = placemark.country ?? "-"
+                self.areaLabel.text = "\(address1), \(city), \(state), \(zipcode), \(country)"
+            }
+        }
+        
         self.mapView.showsUserLocation = true
         self.mapView.setRegion(viewRegion, animated: true)
+    }
+    
+    func geocode(latitude: Double, longitude: Double, completion: @escaping (CLPlacemark?, Error?) -> ()) {
+        CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: latitude, longitude: longitude))
+        {
+            completion($0?.first, $1)
+        }
     }
 }
 
