aea2549277d731eee113ed57804aef1f150a424f
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-01 16:01:28 +0200

    implemented basic login, register and logout functionality

--- iosauthdemo/iosauthdemo/ProfileViewController.swift
+++ iosauthdemo/iosauthdemo/ProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
@@ -23,10 +24,23 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func alertSomething(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func SaveTouch(_ sender: Any) {
     }
     
     @IBAction func LogoutTouch(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "goLogin", sender: self)
+        } catch {
+            alertSomething(title: "Logout Error", message: "Please try again")
+        }
     }
     
     @IBAction func UpdatePhoto(_ sender: Any) {
--- iosauthdemo/iosauthdemo/ViewController.swift
+++ iosauthdemo/iosauthdemo/ViewController.swift
@@ -19,23 +19,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func alertSomething(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
 
     @IBAction func LoginTouch(_ sender: Any) {
+        Auth.auth().signIn(withEmail: TxtUsername.text!, password: TxtPassword.text!) {
+            (user, error) in
+            if error  != nil {
+                self.alertSomething(title: "Login Error", message: "You have failed to log in")
+            } else {
+                self.performSegue(withIdentifier: "goProfile", sender: self)
+            }
+        }
     }
     
     @IBAction func RegisterTouch(_ sender: Any) {
         Auth.auth().createUser(withEmail: TxtUsername.text!, password: TxtPassword.text!) {
             (user, error) in
             if error != nil {
-                let alertController = UIAlertController(title: "Account Error", message: "There was a problem creating your account", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "OK", style: .default)
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
+                self.alertSomething(title: "Account Error", message: "There was a problem creating your account")
             } else {
-                let alertController = UIAlertController(title: "Account Created", message: "Your account was successfully created", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "OK", style: .default)
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
+                self.alertSomething(title: "Account Created", message: "You account was successfully created!")
             }
         }
     }
