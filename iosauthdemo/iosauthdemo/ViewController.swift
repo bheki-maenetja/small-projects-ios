3e90961b666ac0d670bd137bbfdc87614c348058
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-01 14:20:23 +0200

    created new project

---
+++ iosauthdemo/iosauthdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosauthdemo
+//
+//  Created by Bheki Maenetja on 2020/07/01.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
