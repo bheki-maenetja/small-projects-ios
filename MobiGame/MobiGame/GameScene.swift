760a6703a9fb441bb182bd5c39c0eacb83260c58
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-06 14:47:06 +0200

    created new project

---
+++ MobiGame/MobiGame/GameScene.swift
@@ -0,0 +1,54 @@
+//
+//  GameScene.swift
+//  MobiGame
+//
+//  Created by Bheki Maenetja on 2020/07/06.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
