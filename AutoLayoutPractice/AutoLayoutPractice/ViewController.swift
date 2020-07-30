00cdc6d2f91fae89b82c757b193bd758cbc23388
bheki-maenetja <bhekimaenetja@gmail.com>
2020-07-30 11:48:13 +0200

    created project for layout practice

---
+++ AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Bheki Maenetja on 2020/07/30.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
