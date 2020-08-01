0dc881a289f1debce01108ac60a7ef78b7b2c2e8
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-01 12:00:58 +0200

    started implementing game logic

---
+++ ApplePie/ApplePie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Bheki Maenetja on 2020/08/01.
+//  Copyright © 2020 Bonanza. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- ApplePie/ApplePie/ViewController.swift
+++ ApplePie/ApplePie/ViewController.swift
@@ -16,13 +16,26 @@ class ViewController: UIViewController {
     
     @IBOutlet var letterButtons: [UIButton]!
     
+    var currentGame: Game!
+    
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
+    }
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
     }
     
 
