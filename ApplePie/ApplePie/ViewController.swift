aaef590fb0c70b9045fa98ee83b1962f4faf478d
bheki-maenetja <bhekimaenetja@gmail.com>
2020-08-01 11:51:28 +0200

    created outlets and actions

--- ApplePie/ApplePie/ViewController.swift
+++ ApplePie/ApplePie/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImage: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 
 }
 
